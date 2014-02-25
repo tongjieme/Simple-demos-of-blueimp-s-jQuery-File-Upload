5b3c9a125ec8c32b064df192ba432125ac75ff0b
tongjieme <tongjieme@gmail.com>
2014-02-25 16:55:55 +0800

    Remove commented 'console.log'
    Remove commented 'console.log' and fixed one small mistake in file.php

--- file.php
+++ file.php
@@ -1,7 +1,7 @@
 <?php 
 	// echo $_FILES["file"]["tmp_name"];
 	print_r($_FILES);
-	echo '$_FILES["file"]["tmp_name"] upload successfully';
+	echo $_FILES["file"]["tmp_name"].' upload successfully';
 	move_uploaded_file($_FILES["file"]["tmp_name"],
       "upload/" . $_FILES["file"]["name"]);
  ?>
